ca955d6771c3bc4350d0a85e88dce5f5e398c5d1
burhanHere <burhanburewala@gmail.com>
2024-07-05 13:19:41 +0500

    initial Commit

---
+++ Fraction.cs
@@ -0,0 +1,96 @@
+﻿namespace FunctionAndOperatorOverloading
+{
+    public class Fraction
+    {
+        private double numerator;
+        private double denominator;
+
+        public double Numerator { get { return numerator; } set { numerator = value; } }
+        public double Denominator
+        {
+            get { return denominator; }
+            set
+            {
+                denominator = (value == 0) ? throw new ArgumentException("Denominator cannot be 0.") : value;
+            }
+        }
+
+        public Fraction()
+        {
+            numerator = 0.0;
+            denominator = 0.0;
+        }
+
+        public Fraction(double inputNumirator, double inputDenominator)
+        {
+            numerator = inputNumirator;
+            denominator = inputDenominator;
+        }
+
+        public Fraction(Fraction obj)
+        {
+            numerator = obj.numerator;
+            denominator = obj.denominator;
+        }
+
+        public static Fraction Add(Fraction f1, Fraction f2)
+        {
+            double numerator = (f1.denominator == f2.denominator) ?
+                f1.numerator + f2.numerator :
+                (f1.numerator * f2.denominator) + (f2.numerator * f1.denominator);
+
+            double denominator = (f1.denominator == f2.denominator) ?
+                f1.denominator :
+                f1.denominator * f2.denominator;
+
+            return new Fraction(numerator, denominator);
+        }
+
+        public static Fraction Subtract(Fraction f1, Fraction f2)
+        {
+            double numerator = (f1.denominator == f2.denominator) ?
+                f1.numerator - f2.numerator :
+                (f1.numerator * f2.denominator) - (f2.numerator * f1.denominator);
+
+            double denominator = (f1.denominator == f2.denominator) ?
+                f1.denominator :
+                f1.denominator * f2.denominator;
+
+            return new Fraction(numerator, denominator);
+        }
+
+        public static Fraction Multiply(Fraction f1, Fraction f2)
+        {
+            double numerator = f1.numerator * f2.numerator;
+            double denominator = f1.denominator * f2.denominator;
+
+            return new Fraction(numerator, denominator);
+        }
+
+        public double ToDecimal() // Consider using a more descriptive name like "ToDecimal"
+        {
+            return numerator / denominator;
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            return Add(f1, f2);
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            return Subtract(f1, f2);
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            return Multiply(f1, f2);
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FunctionAndOperatorOverloading
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fraction fraction1 = new(5, 3);
+            Console.WriteLine(fraction1.ToString());
+
+            // working with 2 objects only
+
+            Fraction fraction2 = new(5, 2);
+
+            Console.WriteLine($"addFraction function: {Fraction.Add(fraction1, fraction2)}"); // addFraction function
+            Console.WriteLine($"operator + overloaded: {fraction1 + fraction2}");// operator + overloaded
+
+            Console.WriteLine($"SubtractFraction function: {Fraction.Subtract(fraction1, fraction2)}"); // SubtractFraction function
+            Console.WriteLine($"operator - overloaded: {fraction1 - fraction2}");// operator - overloaded
+
+            Console.WriteLine($"MultiplyFraction function: {Fraction.Multiply(fraction1, fraction2)}"); // MultiplyFraction function
+            Console.WriteLine($"operator * overloaded: {fraction1 * fraction2}");// operator * overloaded
+
+            // working with 3 objects only
+
+            Fraction fraction3 = new(2, 5);
+
+            Console.WriteLine($"operator + overloaded: {fraction1 + fraction2 + fraction3}");// operator + overloaded
+
+            Console.WriteLine($"operator - overloaded: {fraction1 - fraction2 - fraction3}");// operator - overloaded
+
+            Console.WriteLine($"operator * overloaded: {fraction1 * fraction2 * fraction3}");// operator * overloaded
+
+            Fraction fraction4 = fraction1;
+            Console.WriteLine("checking if we dont over load operator = what would happen hen:");
+            Console.WriteLine($"fraction4: {fraction4}");
+            Console.WriteLine($"fraction1: {fraction1}");
+            fraction1.Numerator = 60; // this was shallow copy before overloading operator =
+            Console.WriteLine($"After updating valie of fraction1:");
+            Console.WriteLine($"fraction4: {fraction4}");
+            Console.WriteLine($"fraction1: {fraction1}");
+        }
+    }
+}
